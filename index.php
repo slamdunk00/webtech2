8b21594db467c3af4a9f0eb52d00ab9b720e3bbe
LeeYO <bom1442@gmail.com>
2015-04-17 23:25:22 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Web Tech Flash Card</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div class="background">
+		<div class="page">
+			<div class="header">
+                <div class="container" style="height:50px;">
+                    <a href="index.html" id="logo"><img src="images/logo4.png" alt="logo"></a>
+                    <ul>
+                        <li>
+                            <a href="">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+                <form>
+					<input type="text">
+					<input type="submit" value="search">
+				</form>
+			</div>
+			<div class="body home">
+				<div>
+					<div class="featured">
+						<div>
+							<h2>The Most Popular <br> Deck of Day</h2>
+						</div>
+						<a href=""><img src="images/popular.jpg" height="450" width="700"></a>
+					</div>
+					<div>
+						<div>
+							<h3>Latest Deck</h3>
+							<span><a href="">View More Deck</a></span>
+							<ul>
+								<li>
+									<a href=""><img src="images/lastest1.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest1</h4>
+									<p>something about lastest1.</p>
+									<p>By: user1</p>
+								</li>
+								<li>
+									<a href=""><img src="images/lastest2.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest2</h4>
+									<p>something about lastest2.</p>
+									<p>By: user2</p>
+								</li>
+								<li>
+									<a href=""><img src="images/lastest3.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest3</h4>
+									<p>something about lastest3.</p>
+									<p>By: user3</p>
+								</li>
+							</ul>
+							<ul>
+								<li>
+									<a href=""><img src="images/lastest4.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest4</h4>
+									<p>something about lastest4.</p>
+									<p>By: user4</p>
+								</li>
+								<li>
+									<a href=""><img src="images/lastest5.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest5</h4>
+									<p>something about lastest5.</p>
+									<p>By: user5</p>
+								</li>
+								<li>
+									<a href=""><img src="images/lastest6.jpg" alt="" height="152" width="200"></a>
+									<h4>lastest6</h4>
+									<p>something about lastest6.</p>
+									<p>By: user6</p>
+								</li>
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+				<div class="sidebar">
+					<div class="news">
+						<span>Category</span>
+						<ul>
+							<li>
+								<a href="">Language</a> <span>English, Chinese, Japanese, ...</span>
+							</li>
+							<li>
+								<a href="">Country</a> <span>Capital, Travel, Building, ...</span>
+							</li>
+							<li>
+								<a href="">Animal</a> <span>Pet, Wild, Marine, ...</span>
+							</li>
+							<li>
+								<a href="">Car</a> <span>Car, Train, Plane, Ship, ...</span>
+							</li>
+							<li>
+								<a href="">Holiday</a> <span>Festival, Season, Celebration, ...</span>
+							</li>
+							<li>
+								<a href="">Media</a> <span>Movie, Cartoon, Music, ...</span>
+							</li>
+							<li>
+								<a href="">Food</a> <span>Food, Sweet, Drink, ...</span>
+							</li>
+							<li>
+								<a href="">Celebrities</a> <span>Actor, Singer, Athlete, ...</span>
+							</li>
+							<li>
+								<a href="">Sport</a> <span>...</span>
+							</li>
+							<li>
+								<a href="">Other</a> <span>...</span>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<span>Profile</span>
+						<ul>
+							<li>
+								<p>
+									<a href="#">Add Deck</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									<a href="#">All My Deck</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									<a href="#">Favourite Deck</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									<a href="#">Achievements</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									<a href="#">Logout</a>
+								</p>
+							</li>
+						</ul>
+						
+					</div>
+				</div>
+			</div>
+			<div class="footer">
+				<div>
+					<ul>
+						<li class="selected">
+							<a href="index.html">Home</a>|
+						</li>
+						<li>
+							<a href="news.html">News</a>|
+						</li>
+						<li>
+							<a href="team.html">Team</a>|
+						</li>
+						<li>
+							<a href="schedule.html">Schedule</a>|
+						</li>
+						<li>
+							<a href="videos.html">Videos</a>|
+						</li>
+						<li>
+							<a href="about.html">About</a>
+						</li>
+					</ul>
+					<p>
+						&#169; ZZ TIGERS 2023. All Rights Reserved
+					</p>
+				</div>
+				<div class="connect">
+					<span>Follow Us</span> <a href="http://freewebsitetemplates.com/go/facebook/" id="fb">fb</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a> <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">google+</a> <a href="http://www.freewebsitetemplates.com/misc/contact" id="contact">contact</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
